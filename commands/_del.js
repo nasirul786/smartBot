030c41df1fa0f630fd033acfdf12981841afb95b
Captain Cool <148034378+nasirul786@users.noreply.github.com>
2025-02-10 08:08:00 +0530

    Update _del.js

--- commands/_del.js
+++ commands/_del.js
@@ -1,3 +1,16 @@
+/*CMD
+  command: *
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+  group: 
+CMD*/
+
+
 if (request.message?.message_id) {
   Api.deleteMessage({
     message_id: request.message?.message_id
